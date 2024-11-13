d3bda442bef5cc873d1d5b185f85ebb4e525ff56
goldm0ng <jujs45177@gmail.com>
2024-11-13 18:34:39 +0900

    <FIX> 미션 흐름에 따른 비즈니스 로직 메서드 순서 바꾸기

--- src/main/java/roomescape/reservation/business/ReservationService.java
+++ src/main/java/roomescape/reservation/business/ReservationService.java
@@ -1,10 +1,10 @@
-package roomescape.reservation.business;
+package roomescape.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import roomescape.reservation.domain.Reservation;
-import roomescape.reservation.presentation.dto.ReservationDto;
-import roomescape.reservation.persistence.ReservationRepository;
+import roomescape.domain.Reservation;
+import roomescape.dto.ReservationDto;
+import roomescape.repository.ReservationRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,20 +15,16 @@ public class ReservationService {
 
     private final ReservationRepository repository;
 
-    public Reservation addReservation(ReservationDto reservationDto){
-        Reservation reservation = convertToEntity(reservationDto);
-        return repository.save(reservation);
-    }
-
     public List<Reservation> checkReservations(){
         return repository.findAll();
+    }
+
+    public Reservation addReservation(ReservationDto reservationDto){
+        return repository.save(reservationDto);
     }
 
     public Optional<Reservation> deleteReservation(Long reservationId){
         return repository.delete(reservationId);
     }
 
-    private Reservation convertToEntity(ReservationDto dto) {
-        return new Reservation(null, dto.getName(), dto.getDate(), dto.getTime());
-    }
 }
