fe2f464d32597ec3a595101c8e32f1a5344ffa24
goldm0ng <jujs45177@gmail.com>
2024-11-22 10:10:45 +0900

    <FIX> DTO -> Entity 변환 위치 변경에 따른 수정

--- src/main/java/roomescape/reservation/persistence/JdbcReservationRepository.java
+++ src/main/java/roomescape/reservation/persistence/JdbcReservationRepository.java
@@ -1,4 +1,4 @@
-package roomescape.repository;
+package roomescape.reservation.persistence;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -7,8 +7,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-import roomescape.domain.Reservation;
-import roomescape.dto.ReservationDto;
+import roomescape.reservation.domain.Reservation;
 
 import java.sql.PreparedStatement;
 import java.util.List;
@@ -16,26 +15,26 @@ import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
-public class JdbcReservationRepository implements ReservationRepository{
+public class JdbcReservationRepository implements ReservationRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
     @Override
-    public Reservation save(ReservationDto reservationDto) {
+    public Reservation save(Reservation reservation) {
         String sql = "insert into reservation (name, date, time) values (?,?,?)";
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
         jdbcTemplate.update(connection -> {
             PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
-            ps.setString(1, reservationDto.getName());
-            ps.setString(2,reservationDto.getDate());
-            ps.setString(3,reservationDto.getTime());
+            ps.setString(1, reservation.getName());
+            ps.setString(2,reservation.getDate());
+            ps.setString(3,reservation.getTime());
             return ps;
         }, keyHolder);
 
         Long generatedAutoId = keyHolder.getKey().longValue();
-        return new Reservation(generatedAutoId, reservationDto.getName(), reservationDto.getDate(), reservationDto.getTime());
+        return new Reservation(generatedAutoId, reservation.getName(), reservation.getDate(), reservation.getTime());
     }
 
     @Override
