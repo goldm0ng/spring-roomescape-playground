0efc638922efb999acad4b487c08481da3372b6b
goldm0ng <jujs45177@gmail.com>
2024-11-27 13:23:02 +0900

    <FIX> 도메인 객체의 멤버 변수 타입 변경에 따른 Dto -> Entity 변환 로직 수정

--- src/main/java/roomescape/business/ReservationService.java
+++ src/main/java/roomescape/business/ReservationService.java
@@ -3,8 +3,10 @@ package roomescape.business;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import roomescape.domain.Reservation;
+import roomescape.domain.Time;
+import roomescape.persistence.JdbcReservationRepository;
+import roomescape.persistence.JdbcTimeRepository;
 import roomescape.presentation.dto.ReservationDto;
-import roomescape.persistence.ReservationRepository;
 
 import java.util.List;
 
@@ -12,22 +14,29 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReservationService {
 
-    private final ReservationRepository repository;
+    private final JdbcReservationRepository reservationRepository;
+    private final JdbcTimeRepository timeRepository;
 
     public Reservation addReservation(ReservationDto reservationDto) {
-        Reservation reservation = convertToEntity(reservationDto);
-        return repository.save(reservation);
+        Reservation reservation = convertToReservationEntity(reservationDto);
+        return reservationRepository.save(reservation);
     }
 
     public List<Reservation> checkReservations() {
-        return repository.findAll();
+        return reservationRepository.findAll();
     }
 
     public void deleteReservation(Long reservationId) {
-        repository.delete(reservationId);
+        reservationRepository.delete(reservationId);
     }
 
-    private Reservation convertToEntity(ReservationDto reservationDto) {
-        return new Reservation(null, reservationDto.name(), reservationDto.date(), reservationDto.time());
+    private Reservation convertToReservationEntity(ReservationDto reservationDto) {
+        Time time = convertToTimeEntity(reservationDto);
+        return new Reservation(null, reservationDto.name(), reservationDto.date(), time);
+    }
+
+    private Time convertToTimeEntity(ReservationDto reservationDto) {
+        Long timeId = reservationDto.time();
+        return timeRepository.findById(timeId);
     }
 }
